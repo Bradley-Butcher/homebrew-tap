c4109039f2180d512d8c304786bb67859cb43773
Bradley-Butcher <brad@phoebe.ai>
2026-03-13 22:35:14 +0000

    Update changes to v0.2.0

--- Formula/changes.rb
+++ Formula/changes.rb
@@ -2,21 +2,21 @@ class Changes < Formula
   desc "A fast, live-updating terminal UI for visualizing git diffs"
   homepage "https://github.com/Bradley-Butcher/Changes"
   license "MIT"
-  version "0.1.0"
+  version "0.2.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/Bradley-Butcher/Changes/releases/download/v#{version}/changes-aarch64-apple-darwin.tar.gz"
-      sha256 "238ce0fc959737bfcae80a1a95017b38affe5c969c505caab02a7e3319adf8ad"
+      sha256 "b3977badd25ab5cb60bd85deafe73396d1d83d88188bbf4de3e53e7439a5b3fa"
     else
       url "https://github.com/Bradley-Butcher/Changes/releases/download/v#{version}/changes-x86_64-apple-darwin.tar.gz"
-      sha256 "fb3ea11132bfb72fa96bd2eebe75c3d7f9d796137ced730567740d994f2e9c8a"
+      sha256 "76971ffff217971cf49dd89b152efe1e2af7cf901baca2a7dc012c3154880e73"
     end
   end
 
   on_linux do
     url "https://github.com/Bradley-Butcher/Changes/releases/download/v#{version}/changes-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "242ded4e0868273c81c5b171a1a2ff6c1c9f09752a4a7f43c623cae4b380aa04"
+    sha256 "8c42090d5772849f5ca9c1539f402b1261bf8c1962b084bdcd421fd7db03d453"
   end
 
   def install
