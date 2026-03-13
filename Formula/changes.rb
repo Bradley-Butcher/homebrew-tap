83e6526c6b9070b059072a9e0b9b11141ba4571b
Bradley-Butcher <brad@phoebe.ai>
2026-03-13 21:21:53 +0000

    Update changes formula with v0.1.0 sha256 checksums

--- Formula/changes.rb
+++ Formula/changes.rb
@@ -7,13 +7,16 @@ class Changes < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/Bradley-Butcher/Changes/releases/download/v#{version}/changes-aarch64-apple-darwin.tar.gz"
+      sha256 "238ce0fc959737bfcae80a1a95017b38affe5c969c505caab02a7e3319adf8ad"
     else
       url "https://github.com/Bradley-Butcher/Changes/releases/download/v#{version}/changes-x86_64-apple-darwin.tar.gz"
+      sha256 "fb3ea11132bfb72fa96bd2eebe75c3d7f9d796137ced730567740d994f2e9c8a"
     end
   end
 
   on_linux do
     url "https://github.com/Bradley-Butcher/Changes/releases/download/v#{version}/changes-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "242ded4e0868273c81c5b171a1a2ff6c1c9f09752a4a7f43c623cae4b380aa04"
   end
 
   def install
